7f184ceca3ef696f7424e733afb1c1e079cd9155
blackcult <jan.novak@outlook.com>
2020-10-30 16:42:48 +0100

    working on credentials

--- ec2_sg.py
+++ ec2_sg.py
@@ -1,23 +1,35 @@
 import argparse
-from nslookup import Nslookup
 import requests
 import csv
 import boto3
 
+CRED = ['file',
+        'parameters',
+        'host']
 
-credentials_file="sg_test_eun1.csv"
+global client
+client = boto3.client('ec2')
 
-#client = boto3.client(
-#                'route53',
-#                aws_access_key_id=args.access_key,
-#                aws_secret_access_key=args.secret_key
-#                )
+def credentials():
+    client = boto3.client('ec2')
+    client = boto3.client('ec2',
+                           aws_access_key_id=args.access_key,
+                           aws_secret_access_key=args.secret_key
+                         )
+
+def read_sg():
+
+
+def public_ip():
+    ip = requests.get('https://checkip.amazonaws.com').text.strip()
+    return ip
 
 def main():
     parser = argparse.ArgumentParser(description='Create DNS records on R53')
     #parser.add_argument('--run', type=str, choices=RUN, required=False)
     parser.add_argument('--name', type=str)
-    #parser.add_argument('--type', choices=TYPE, type=str)
+    parser.add_argument('--cred', choices=CRED, type=str)
+    parser.add_argument('--file', type=str)
     parser.add_argument('--value')
     #parser.add_argument('--action', choices=ACTION)
     parser.add_argument('--ttl', default=300, type=int)
@@ -28,10 +40,12 @@ def main():
 
     args = parser.parse_args()
 
-    with open(credentials_file, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            print(', '.join(row))
+    with open(args.file, newline='') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            print(row['Access key ID'])
+            print(row['Secret access key'])
+        
 
 
 

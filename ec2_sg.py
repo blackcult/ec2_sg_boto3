0db3df1df4ab4cfee704ff350ceae52a8cc41f36
blackcult <jan.novak@outlook.com>
2020-12-25 21:40:15 +0100

    new update

--- ec2_sg.py
+++ ec2_sg.py
@@ -2,49 +2,65 @@ import argparse
 import requests
 import csv
 import boto3
-
-CRED = ['file',
-        'parameters',
-        'host']
-
-global client
-client = boto3.client('ec2')
-
-def credentials():
-    client = boto3.client('ec2')
-    client = boto3.client('ec2',
-                           aws_access_key_id=args.access_key,
-                           aws_secret_access_key=args.secret_key
-                         )
-
-def read_sg():
-
+from botocore.exceptions import ClientError
 
 def public_ip():
     ip = requests.get('https://checkip.amazonaws.com').text.strip()
     return ip
+    
+def allow_ingress(client, port, ip, comment, sg, udptcp):
+    try:
+        data = client.authorize_security_group_ingress(
+        GroupId=sg,
+        IpPermissions=[
+            {'IpProtocol': udptcp,
+             'FromPort': port,
+             'ToPort': port,
+             'IpRanges': [
+                {'CidrIp': ip,
+                'Description': comment}
+             ]
+            }
+        ])
+        print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
+
+def clear_ingress(resource, sg):
+    group = resource.SecurityGroup(sg)
+    group.revoke_ingress(IpPermissions=group.ip_permissions)
 
 def main():
-    parser = argparse.ArgumentParser(description='Create DNS records on R53')
-    #parser.add_argument('--run', type=str, choices=RUN, required=False)
-    parser.add_argument('--name', type=str)
-    parser.add_argument('--cred', choices=CRED, type=str)
-    parser.add_argument('--file', type=str)
-    parser.add_argument('--value')
-    #parser.add_argument('--action', choices=ACTION)
-    parser.add_argument('--ttl', default=300, type=int)
-    parser.add_argument('--zone', type=str)
-    parser.add_argument('--spec', default=False, type=bool)
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--port', type=int)
+    parser.add_argument('--udptcp', type=str)
+    parser.add_argument('--region', type=str)
+    parser.add_argument('--description', type=str)
     parser.add_argument('--access_key', type=str)
     parser.add_argument('--secret_key', type=str)
+    parser.add_argument('--sg', type=str)
 
     args = parser.parse_args()
+    session = boto3.session.Session(aws_access_key_id=args.access_key,
+                                    aws_secret_access_key=args.secret_key,
+                                    region_name = args.region
+                                    )
 
-    with open(args.file, newline='') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        for row in csv_reader:
-            print(row['Access key ID'])
-            print(row['Secret access key'])
+    ec2 = session.client('ec2')
+    ec2_resource = session.resource('ec2')
+    publicip = public_ip()
+
+    with open("publicip.txt", "r") as handler:
+        publicip_writen = handler.read()
+
+    if publicip != publicip_writen:
+        print(f'IP {publicip_writen} changed to {publicip} - updating {args.sg}')
+        clear_ingress(ec2_resource, args.sg)
+        allow_ingress(ec2, args.port, f"{publicip}/32", args.description, args.sg, args.udptcp)
+        with open("publicip.txt", "w") as writer:
+            writer.write(publicip)
+    else:
+        print(f'Public IP is still {publicip_writen}')
         
 
 

ceb277634428b4e0112994bbcd04b31cd7b97727
blackcult <jan.novak@outlook.com>
2020-12-26 13:55:30 +0100

    new version

--- ec2_sg_curl.py
+++ ec2_sg_curl.py
@@ -1,9 +1,9 @@
 import argparse
-import requests
 import boto3
 from botocore.exceptions import ClientError
 import subprocess
 import json
+import random
 
 def public_ip(interface):
     ip = subprocess.getoutput(f'curl -s --interface {interface} https://checkip.amazonaws.com')
@@ -23,7 +23,6 @@ def allow_ingress(client, port, ip, comment, sg, udptcp):
              ]
             }
         ])
-        #print('Ingress Successfully Set %s' % data)
     except ClientError as e:
         print(e)
 
@@ -33,14 +32,13 @@ def clear_ingress(resource, sg):
 
 def main():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--port', type=int)
-    parser.add_argument('--udptcp', type=str)
     parser.add_argument('--region', type=str)
-    parser.add_argument('--description', type=str)
     parser.add_argument('--access_key', type=str)
     parser.add_argument('--secret_key', type=str)
     parser.add_argument('--sg', type=str)
     parser.add_argument('--interface', type=str)
+    parser.add_argument('--init')
+    parser.add_argument('--rulesfile', type=str)
 
     args = parser.parse_args()
     session = boto3.session.Session(aws_access_key_id=args.access_key,
@@ -52,25 +50,25 @@ def main():
     ec2_resource = session.resource('ec2')
     publicip = public_ip(args.interface)
 
-    with open("rules.json", "r") as handler:
+    with open(args.rulesfile, "r") as handler:
         json_file = json.load(handler)
 
-    if publicip != json_file["previousIp"]:
+    if args.init:
+        json_file["previousIp"] = ""
+        with open(args.rulesfile, "w") as writer:
+            writer.write(json.dumps(json_file,indent=4))
+    elif publicip != json_file["previousIp"]:
         print(f'IP {json_file["previousIp"]} changed to {publicip} - updating {args.sg}')
-        allow_ingress(ec2, 999, f"{publicip}/32", "temp rule", args.sg, "udp")
+        allow_ingress(ec2, random.randint(1000, 9999), f"{publicip}/32", "temp rule", args.sg, "udp")
         clear_ingress(ec2_resource, args.sg)
         for rule in json_file["rules"]:        
             allow_ingress(ec2, rule["port"], f"{publicip}/32", rule["description"], args.sg, rule["type"])
         json_file["previousIp"] = publicip
-        with open("rules.json", "w") as writer:
+        with open(args.rulesfile, "w") as writer:
             writer.write(json.dumps(json_file,indent=4))
-            # json.dump(json_file, writer)
     else:
         print(f'Public IP is still {json_file["previousIp"]}')
         
-
-
-
 
 if __name__ == "__main__":
     main()
